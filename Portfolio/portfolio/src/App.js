46d45f9d2f01faf239cda010e86cb7643d397e18
Ayoub Berhili <ayoub.berhili@gmail.com>
2020-04-27 08:37:21 +0200

    Major update

--- Portfolio/portfolio/src/App.js
+++ Portfolio/portfolio/src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import styles from './App.module.css'
 
+import './App.css'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-import {} from '@material-ui/core';
-
-// eslint-disable-next-line
+ // eslint-disable-next-line
 import {ProjectCard, SkillCard, SocialLinkCard, Header} from './components'
 
 
 class App extends Component {
+    state = {
+        logoUrl : "",
+        heroUrl : "",
+        title : "<Aybrl/>",
+        description : "Android and Back-end Developer"
+    }
     render(){
         return (
             <div>
-                <Header></Header>
-                <div className={styles.container}>
-                    <div className="row">
-                        <ProjectCard></ProjectCard>
-                    </div>
+                <div className="hero-image" style={style.heroStyle}>
+                    <Header/>
+                    <header className="hero-text">
+                        <h2 id="title">{this.state.title}</h2>
+                        <h4 id="descTitle">{this.state.description}</h4>
+                    </header>
                 </div>
+                <ProjectCard></ProjectCard>
             </div>
         );
+    }
+}
+
+const style = {
+    heroStyle : { 
+        'min-height': '520px',
+        'min-width': '100%', 
     }
 }
 
